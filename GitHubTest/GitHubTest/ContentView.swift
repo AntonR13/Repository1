046207c053731f5034017dbcf7835c145796a99a
AntonR13 <167611070+AntonR13@users.noreply.github.com>
2024-04-20 17:38:47 +0300

    Create

---
+++ GitHubTest/GitHubTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubTest
+//
+//  Created by АнтохаПрограммист on 20.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding(90)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubTest/GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by АнтохаПрограммист on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
